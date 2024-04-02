ff4dbeca491780636ac1617527f1599fb83898da
elipeter <elicpeter@gmail.com>
2024-04-02 17:07:42 -0400

    Added a text engine

--- UI/TextEngine.cpp
+++ UI/TextEngine.cpp
@@ -1,5 +1,54 @@
-//
-// Created by Eli Peter23 on 5/4/23.
-//
-
 #include "TextEngine.h"
+
+TextEngine::TextEngine() noexcept {
+    // Initialize SDL_ttf
+    ASSERT(TTF_Init() == 0, "Failed to initialize SDL_ttf");
+
+    // Get the renderer from somewhere
+    renderer_ = nullptr;
+}
+
+TextEngine::~TextEngine() {
+    // Quit SDL_ttf
+    do {
+        TTF_Quit();
+    } while (TTF_WasInit());
+}
+
+bool TextEngine::init(SDL_Renderer *renderer) {
+    // Load your fonts and perform any initialization here
+    this->renderer_ = renderer;
+    ASSERT(renderer_, "Renderer is null");
+
+    font = loadFont(DEFAULT_FONT, 24);
+    return true;
+}
+
+void TextEngine::renderText(const std::string& text, int x, int y, int fontSize, SDL_Color color) {
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer_, surface);
+
+    SDL_Rect destRect = {x, y, surface->w, surface->h};
+    SDL_RenderCopy(renderer_, texture, nullptr, &destRect);
+
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+}
+
+TTF_Font* TextEngine::loadFont(const std::string& fontPath, int fontSize) {
+    font = TTF_OpenFont(fontPath.c_str(), fontSize);
+    ASSERT(font != nullptr, "Failed to load font: %s\n", TTF_GetError());
+    return font;
+}
+
+void TextEngine::free() {
+    TTF_CloseFont(font);
+    font = nullptr;
+    renderer_ = nullptr;
+}
+
+void TextEngine::updateFont(const std::string& fontPath, int fontSize=24) {
+    TTF_CloseFont(font);
+    font = loadFont(fontPath, fontSize);
+}
+
--- UI/TextEngine.h
+++ UI/TextEngine.h
@@ -1,5 +1,27 @@
 #pragma once
 
-class TextEngine {
-};
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <string>
+#include "../util/datatypes.h"
 
+#define DEFAULT_FONT "assets/fonts/Roboto-Regular.ttf"
+
+class TextEngine {
+public:
+    TextEngine();
+    ~TextEngine();
+
+    bool init(SDL_Renderer *renderer_);
+    void renderText(const std::string& text, int x, int y, int fontSize, SDL_Color color);
+    void updateFont(const std::string& fontPath, int fontSize);
+    void free();
+
+private:
+    TTF_Font* loadFont(const std::string& fontPath, int fontSize);
+    TTF_Font* font;
+    SDL_Renderer *renderer_{};
+
+
+
+};
